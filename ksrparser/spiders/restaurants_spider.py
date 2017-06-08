ee71d172050af5e7bd61680408672bbb8c301eee
Nikolay Baychenko <nikolay.baychenko@gmail.com>
2017-06-08 20:49:05 +0300

    Add regions and cities

--- ksrparser/spiders/restaurants_spider.py
+++ ksrparser/spiders/restaurants_spider.py
@@ -4,37 +4,40 @@ import scrapy
 class RestaurantSpider(scrapy.Spider):
     name = 'restaurant'
 
-    start_urls = ['http://www.kela.fi/web/en/meal-subsidy-student-restaurants']
+    # In English
+    # start_urls = ['http://www.kela.fi/web/en/meal-subsidy-student-restaurants']
+    # In Finnish
+    start_urls = ['http://www.kela.fi/ateriatuki-opiskelijaravintolahaku']
     region_base_url = 'http://www.kela.fi'
     restaurants_base_url = 'https://asiointi.kela.fi/opiskelijaravintolahaku/'
 
-    # TODO: pass region name
     def parse(self, response):
-        # follow links by regions
-        regions = response.css('section.kelafi-article ul')[0].css('li a::attr(href)').extract()
-        for href in regions:
-            yield response.follow(self.region_base_url + href, self.parse_cities)
+        regions = response.css('section.kelafi-article ul')[0].css('li')
+        for region in regions:
+            region_name = region.css('a::text').extract_first()
+            region_href = region.css('a::attr(href)').extract_first()
+            yield response.follow(self.region_base_url + region_href, self.parse_cities, meta={'region_name': region_name})
 
-    # TODO: pass city name
     def parse_cities(self, response):
-        # follow links by cities
-        cities = response.css('section.kelafi-article ul')[0].css('li a::attr(href)').extract()
-        for href in cities:
-            yield response.follow(href, self.parse_restaurants)
+        region_name = response.meta['region_name']
+        cities = response.css('section.kelafi-article ul')[0].css('li')
+        for city in cities:
+            city_name = city.css('a::text').extract_first()
+            city_href = city.css('a::attr(href)').extract_first()
+            yield response.follow(city_href, self.parse_restaurants, meta={'region_name': region_name, 'city_name': city_name})
 
     def parse_restaurants(self, response):
-        # follow links by restaurants
+        region_name = response.meta['region_name']
+        city_name = response.meta['city_name']
         restaurants = response.css('table a::attr(href)').extract()
         for href in restaurants:
-            yield response.follow(self.restaurants_base_url + href, self.parse_restaurant)
+            yield response.follow(self.restaurants_base_url + href, self.parse_restaurant, meta={'region_name': region_name, 'city_name': city_name})
 
     def parse_restaurant(self, response):
-
-        #def extract_with_css(query):
-        #    return response.css(query).extract_first().strip()
-
         yield {
             'restaurant': response.css('table tr td b::text').extract_first().strip(),
+            'region': response.meta['region_name'],
+            'city': response.meta['city_name'],
             'street_address': response.css('table tr')[1].css('td::text')[2].extract().strip(),
             'postal_code': response.css('table tr')[2].css('td::text')[2].extract().strip(),
             'post_office': response.css('table tr')[3].css('td::text')[2].extract().strip(),
